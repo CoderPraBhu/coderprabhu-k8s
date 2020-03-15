9fd94771871ddeab06f3512119c84dbc97ad3f75
CoderPraBhu <prashantbhuruk88@gmail.com>
2020-03-14 18:09:39 -0700

    ui deployment for clickable logo

---
+++ curlscript.bat
@@ -0,0 +1,21 @@
+﻿
+curl http://coderprabhu.com
+echo "-------------"
+curl https://coderprabhu.com
+echo "-------------"
+curl http://www.coderprabhu.com
+echo "-------------"
+curl https://www.coderprabhu.com
+echo "-------------"
+curl http://api.coderprabhu.com
+echo "-------------"
+curl https://api.coderprabhu.com
+echo "-------------"
+curl http://api.coderprabhu.com/hello
+echo "-------------"
+curl https://api.coderprabhu.com/hello
+echo "-------------"
+curl http://api.coderprabhu.com/count
+echo "-------------"
+curl https://api.coderprabhu.com/count
+echo "-------------"
